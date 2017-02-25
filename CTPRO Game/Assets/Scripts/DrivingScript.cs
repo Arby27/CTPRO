768a86469827216a0d1f84acf5a4c0c634fe5606
Arby27 <up723163@myport.ac.uk>
2017-02-25 17:05:22 +0000

    commit

--- CTPRO Game/Assets/Scripts/DrivingScript.cs
+++ CTPRO Game/Assets/Scripts/DrivingScript.cs
@@ -4,7 +4,10 @@ using System.Collections;
 
 public class DrivingScript : MonoBehaviour {
 
-   public  WheelCollider frontWheel;
+     
+    public static float fastestSpeed;
+
+    public  WheelCollider frontWheel;
 
    public  WheelCollider backWheel;
 
@@ -20,27 +23,49 @@ public class DrivingScript : MonoBehaviour {
     float zRot;
     float yRot;
 
+    float acceleration;
+    float force;
+    float distance;
+    double intialVel;
+    double CurrentSpeed;
+
     Quaternion currentRot;
     Quaternion targetRot;
+
+
+    Vector3 lastPos;
+    Vector3 currentPos;
 
 	// Use this for initialization
 	void Start () {
         
         transform.Rotate(new Vector3(0, 90, 0));
-        
+        lastPos = currentPos;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         
-            if (Input.GetButtonDown("Fire1")|| OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger)> 0.0f)
+        if (Input.GetButton("Fire1"))
         {
             print("pressed");
-            backWheel.motorTorque += moveSpeed ;
+            acceleration += 0.1f;
             backWheel.brakeTorque = 0;
         }
-        if(Input.GetButtonDown("Reload"))
+        force = backWheel.mass * acceleration;
+       // GetComponent<Rigidbody>().AddRelativeForce(new Vector3(0, 0, force));
+       
+        GetComponent<Rigidbody>().AddForce(transform.forward * force, ForceMode.Acceleration);
+
+        if (acceleration >= 0)
         {
+            acceleration -= 0.05f;
+        }
+
+        if (Input.GetButton("Reload"))
+        {
+            acceleration -=  0.09f;
+
             if (backWheel.motorTorque < 0)
             {
                 backWheel.brakeTorque = brakeSpeed;
@@ -56,6 +81,20 @@ public class DrivingScript : MonoBehaviour {
             }
         }
 
+        currentPos = transform.position;
+        distance = Vector3.Distance(currentPos,lastPos);
+        lastPos = currentPos;
+
+
+        CurrentSpeed = GetComponent<Rigidbody>().velocity.magnitude;
+        CurrentSpeed = (CurrentSpeed * 1609.344) / 3600;
+
+        if(CurrentSpeed > fastestSpeed)
+        {
+            fastestSpeed = (float)CurrentSpeed;
+        }
+
+
 
         frontWheel.steerAngle = Input.GetAxis("Horizontal") * turnAngle;
 
@@ -65,7 +104,7 @@ public class DrivingScript : MonoBehaviour {
         transform.eulerAngles =  new Vector3(0, 90, zRot);
         Bike.transform.eulerAngles = new Vector3(0, -yRot, zRot);
 
-        Speed.text = backWheel.motorTorque.ToString() + "MPH";
+        Speed.text = CurrentSpeed.ToString() + "MPH";
 
 
         Bike.transform.rotation = Quaternion.Slerp(Quaternion.Euler(transform.rotation.eulerAngles.x, 
@@ -75,11 +114,7 @@ public class DrivingScript : MonoBehaviour {
                                                                     Time.deltaTime);
         
 
-    
-        
-
-     
-
+   
 
     }
 }
